8f6904552cf25db98e469f6e2f44d5082c004405
1104-Etchegoyhen-Vytis <158015712+1104-Etchegoyhen-Vytis@users.noreply.github.com>
2024-04-29 11:50:39 -0700

    Add files via upload
    boom

---
+++ Image.c
@@ -0,0 +1,45 @@
+//Vytis Etchegoyhen & Jaeiz Ocampo
+
+#include <stdio.h>
+#define FILE_NAME "image.txt"
+//index
+
+//================================================================================================================================
+int main(){
+	int image[1000][1000];
+	
+	menu();
+	
+	
+	
+
+
+
+
+
+
+	return 0;
+}
+void menu(){
+	char choice;
+
+	printf("***Image Workspace***\n");
+	printf("1: Load New Image\n");
+	printf("2: Display Current Image\n");
+	printf("3: Edit Current Image\n");
+	printf("4: Exit Program\n");
+	scanf("%d", &choice);
+	
+	if (choice == '1'){
+	
+	}else if (choice == 2){
+	
+	}else if (choice == 3){
+	
+	}else if (choice == 4){
+	
+	}else{
+		 printf("Invalid Input");
+	}
+
+}

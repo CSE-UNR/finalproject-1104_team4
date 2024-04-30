af79cc7bfe49422869e41c99d19dea22d6696866
1104-Etchegoyhen-Vytis <158015712+1104-Etchegoyhen-Vytis@users.noreply.github.com>
2024-04-30 13:58:39 -0700

    Add files via upload
    bit of work done. Most of the menu functionality is here now (none of the loops or functions are in place)

--- Image.c
+++ Image.c
@@ -1,45 +1,61 @@
 //Vytis Etchegoyhen & Jaeiz Ocampo
 
 #include <stdio.h>
-#define FILE_NAME "image.txt"
+#define FILE_NAME "image_file.txt"
 //index
 
-//================================================================================================================================
+//=====================================================================================================================
 int main(){
-	int image[1000][1000];
-	
-	menu();
-	
-	
-	
-
-
-
-
-
-
-	return 0;
-}
-void menu(){
+	char image[1000][1000];
 	char choice;
 
-	printf("***Image Workspace***\n");
-	printf("1: Load New Image\n");
-	printf("2: Display Current Image\n");
-	printf("3: Edit Current Image\n");
-	printf("4: Exit Program\n");
-	scanf("%d", &choice);
+	printf("**ERINSTAGRAM**\n");
+	printf("1: Load image\n");
+	printf("2: Display image\n");
+	printf("3: Edit image\n");
+	printf("0: Exit Program\n");
+	printf("\nChoose from one of the options above: ");
+	scanf("%c", &choice);
 	
 	if (choice == '1'){
-	
-	}else if (choice == 2){
-	
-	}else if (choice == 3){
-	
-	}else if (choice == 4){
-	
+		//load new image	
+		
+	}else if (choice == '2'){
+		//display current image
+		
+		
+	}else if (choice == '3'){
+		//edit current image
+		printf("**Edit Current Image**\n");
+		printf("1: Crop image\n");
+		printf("2: Dim image\n");
+		printf("3: Brighten image\n");
+		printf("0: Return to main menu\n");
+		printf("\nChoose from one of the options above: ");
+		scanf(" %c", &choice);
+		
+		if (choice == '1'){
+			//crop
+			
+		}else if (choice == '2'){
+			//dim
+			
+		}else if (choice == '3'){
+			//lighten
+			
+		}else if (choice == '4'){
+			//rotate 90*
+			
+		}else{
+			printf("\nGoodbye!\n\n");
+			return 0;
+		}
 	}else{
-		 printf("Invalid Input");
+		printf("\nGoodbye!\n\n");
+		return 0;
 	}
-
+	
+return 0;
 }
+//=====================================================================================================================
+

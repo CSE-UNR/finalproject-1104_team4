587be038ba10563d9cf1f676edc07584326de33d
1104-Ocampo-Jaeiz <jaeizo@nevada.unr.edu>
2024-05-01 04:22:03 +0000

    not done yet

--- Image.c
+++ Image.c
@@ -1,14 +1,21 @@
 //Vytis Etchegoyhen & Jaeiz Ocampo
 
 #include <stdio.h>
+#define MAX_SIZE 1000
 #define FILE_NAME "image_file.txt"
 //index
 
 //=====================================================================================================================
+void load(int rowsize, int colsize, int array[][colsize], FILE* fptr);
+void display(int rowsize, int colsize, int array[][colsize], FILE* fptr);
 int main(){
-	char image[1000][1000];
+	//char image[1000][1000];
+	int image[MAX_SIZE][MAX_SIZE];
 	char choice;
-
+	FILE* imagefileptr;
+	
+	imagefileptr = fopen(FILE_NAME, "r");
+	
 	printf("**ERINSTAGRAM**\n");
 	printf("1: Load image\n");
 	printf("2: Display image\n");
@@ -54,8 +61,20 @@ int main(){
 		printf("\nGoodbye!\n\n");
 		return 0;
 	}
-	
+
+fclose(imagefileptr);	
 return 0;
+}
+void load(int rowsize, int colsize, int array[][colsize], FILE* fptr){
+	for(int i = 0; i < rowsize; i++){
+		for(int j = 0; j < colsize; j++){
+			fscanf(fptr, "%d", array[i][j]);
+		}
+	}
+}
+void display(int rowsize, int colsize, int array[][colsize], FILE* fptr){
+	char convertImage[MAX_SIZE][MAX_SIZE];
+	
 }
 //=====================================================================================================================
 

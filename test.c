b0e2671a828fd52cbe09016d3e2032e1a2a58a8d
1104-Ocampo-Jaeiz <jaeizo@nevada.unr.edu>
2024-05-07 02:18:31 +0000

    almost there

--- test.c
+++ test.c
@@ -5,32 +5,37 @@
 #define Y_SIZE 100
 #define X_SIZE 100
 
-#define FILE_NAME "test_image.txt"
+//#define FILE_NAME "test_image.txt"
+
 //index
 void load(char image_array[][X_SIZE], FILE* imagefileptr, int *colm, int *row);
-void display(char image_array[][X_SIZE], FILE* imagefileptr, int *colm);
-void crop(int rowSize, int colSize, char image_array[][X_SIZE]);
+void display(char chararray[][X_SIZE], char image_array[][X_SIZE], FILE* imagefileptr, int *colm);
+void dim(char temparray[][X_SIZE], char chararray[][X_SIZE], char image_array[][X_SIZE], FILE* imagefileptr, int *colm, char *filename);
+void brighten(char temparray[][X_SIZE], char chararray[][X_SIZE], char image_array[][X_SIZE], FILE* imagefileptr, int *colm, char *filename);
+void crop(char chararray[][X_SIZE], char image_array[][X_SIZE], FILE* imagefileptr, int *colm, char *filename, int rowSize, int colSize);
 //=====================================================================================================================
 int main(){
 	char image_array[Y_SIZE][X_SIZE];
+	char chararray[Y_SIZE][X_SIZE];
+	char temparray[Y_SIZE][X_SIZE];
 	int colm, row;
-	char choice;
+	int choice;
 	
 	int pass = 0;
 	char filename[100];
 	
 	FILE* imagefileptr;
 	
-	while(pass == 0){
+	for(int loop = 1; loop > 0; loop++){
 		printf("**ERINSTAGRAM**\n");
 		printf("1: Load image\n");
 		printf("2: Display image\n");
 		printf("3: Edit image\n");
 		printf("0: Exit Program\n");
 		printf("\nChoose from one of the options above: ");
-		scanf(" %c", &choice);
+		scanf(" %d", &choice);
 		
-		if (choice == '1'){
+		if (choice == 1){
 			//load new image
 			printf("What is the name of the image file? ");
 			scanf("%s", filename);
@@ -38,51 +43,62 @@ int main(){
 			
 			if (imagefileptr != NULL){
 				load(image_array, imagefileptr, &colm, &row);
-				
+				fclose(imagefileptr);
+				pass = 1;
 			}else{
 				printf("Could not find an image with that filename.\n");
 				
 			}
-		}else if (choice == '2'){
+		}else if (choice == 2){
 			//display current image
-			display(image_array, imagefileptr, &colm);
-			
-		}else if (choice == '3'){
+			printf("\n");
+			display(chararray, image_array, imagefileptr, &colm);
+			printf("\n");
+		}else if (choice == 2 && pass == 0){
+			printf("Sorry, no image to display\n\n");
+				
+		}else if (choice == 3 && pass == 1){
 			//edit current image
-			printf("**Edit Current Image**\n");
+			printf("\n\n**EDITING**\n");
 			printf("1: Crop image\n");
 			printf("2: Dim image\n");
 			printf("3: Brighten image\n");
 			printf("0: Return to main menu\n");
 			printf("\nChoose from one of the options above: ");
-			scanf(" %c", &choice);
+			scanf(" %d", &choice);
 			
-			if (choice == '1'){
+			if (choice == 1){
 				//crop
-				display(image_array, imagefileptr, &colm);
-				crop(row, colm, image_array);
+				display(chararray, image_array, imagefileptr, &colm);
+				crop(chararray, image_array, imagefileptr, &colm, filename, row, colm);
 				
-				
-			}else if (choice == '2'){
+			}else if (choice == 2){
 				//dim
+				dim(temparray, chararray, image_array, imagefileptr, &colm, filename);
 				
-			}else if (choice == '3'){
-				//lighten
+			}else if (choice == 3){
+				//brighten
+				brighten(temparray, chararray, image_array, imagefileptr, &colm, filename);
 				
-			}else if (choice == '4'){
-				//rotate 90*
+			}else if (choice == 0){
+				//main menu
 				
 			}else{
-				printf("\nGoodbye!\n\n");
-				return 0;
+				printf("Invalid option, please try again.");
 			}
-		}else{
+		}else if (choice == 3 && pass == 0){
+			printf("Sorry, no image to edit\n\n");
+			
+		}else if (choice == 0){
 			printf("\nGoodbye!\n\n");
 			return 0;
+			
+		}else{
+			printf("Invalid option, please try again.\n");
+			
 		}
 	}
-
-	fclose(imagefileptr);	
+	
 return 0;
 }
 //=====================================================================================================================
@@ -93,7 +109,8 @@ void load(char image_array[][X_SIZE], FILE* imagefileptr, int *colm, int *row){
 			fscanf(imagefileptr, "%c", &image_array[j][i]);
 			if(image_array[j][0] == '\n' || image_array[j][0] == '\0'){
 				*colm = j;
-				goto end;
+				printf("\nImage successfully loaded!\n\n");
+				return;
 				
 			}else if(image_array[j][i] == '\n' || image_array[j][i] == '\0'){
 				*row = i;
@@ -101,28 +118,208 @@ void load(char image_array[][X_SIZE], FILE* imagefileptr, int *colm, int *row){
 			}
 		}
 	}
-	end:
-	printf("\nImage successfully loaded!\n\n");
 	return;
 }
-void display(char image_array[][X_SIZE], FILE* imagefileptr, int *colm){
-	
+void display(char chararray[][X_SIZE], char image_array[][X_SIZE], FILE* imagefileptr, int *colm){
+	//convert image array to char array
 	for(int j = 0; j < *colm; j++){
-		printf("\n");
 		for(int i = 0; i >= 0; i++){
-			printf("%c", image_array[j][i]);
+			if (image_array[j][i] == '0'){
+				chararray[j][i] = ' ';
+			}else if (image_array[j][i] == '1'){
+				chararray[j][i] = '.';
+			}else if (image_array[j][i] == '2'){
+				chararray[j][i] = 'o';
+			}else if (image_array[j][i] == '3'){
+				chararray[j][i] = 'O';
+			}else if (image_array[j][i] == '4'){
+				chararray[j][i] = '0';
+			}
+			
 			if(image_array[j][i] == '\n' || image_array[j][i] == '\0'){
+				chararray[j][i] = image_array[j][i];
 				break;
 			}
 		}
 	}
-	printf("\n\n");
+	//print character array
+	for(int j = 0; j < *colm; j++){
+		if (j != *colm && j != 0){
+			printf("\n");
+		}
+		for(int i = 0; i >= 0; i++){
+			printf("%c", chararray[j][i]);
+			if(chararray[j][i] == '\n' || chararray[j][i] == '\0'){
+				break;
+			}
+		}
+	}
+	
+	printf("\n");
 return;
 }
-void crop(int rowSize, int colSize, char image_array[][X_SIZE]){
+void dim(char temparray[][X_SIZE], char chararray[][X_SIZE], char image_array[][X_SIZE], FILE* imagefileptr, int *colm, char *filename){
+	char choose;
+	//transer image_array to temparray
+	for(int j = 0; j < *colm; j++){
+		for(int i = 0; i >= 0; i++){
+			temparray[j][i] = image_array[j][i];
+			if(temparray[j][i] == '\n' || temparray[j][i] == '\0'){
+				break;
+			}
+		}
+	}
+	//dim image_array	
+	for(int j = 0; j < *colm; j++){
+		for(int i = 0; i >= 0; i++){
+			if (temparray[j][i] == '0'){
+				image_array[j][i] = '0';
+			}else if (temparray[j][i] == '1'){
+				image_array[j][i] = '0';
+			}else if (temparray[j][i] == '2'){
+				image_array[j][i] = '1';
+			}else if (temparray[j][i] == '3'){
+				image_array[j][i] = '2';
+			}else if (temparray[j][i] == '4'){
+				image_array[j][i] = '3';
+			}
+			
+			if(temparray[j][i] == '\n' || temparray[j][i] == '\0'){
+				image_array[j][i] = temparray[j][i];
+				break;
+			}
+		}
+	}
 	
-	char newArray[Y_SIZE][X_SIZE];
+	display(chararray, image_array, imagefileptr, colm);
+	printf("Would you like to save the file? (y/n) ");
+	scanf(" %c", &choose);
+	if(choose == 'y'){
+	
+		FILE* save;
+		
+		printf("What do you want to name the image file? (include the extension) ");
+		scanf("%s", filename);
+		save = fopen(filename, "w");
+		char newline = '\n';
+		for(int j = 0; j < *colm; j++){
+			for(int i = 0; i >= 0; i++){
+				fprintf(save, "%c", chararray[j][i]);
+				if(chararray[j][i] == '\n' || chararray[j][i] == '\0'){
+					break;
+				}
+			}
+		}
+		printf("\nImage successfully saved!\n\n");
+		fclose(save);
+		
+		for(int j = 0; j < *colm; j++){
+			for(int i = 0; i >= 0; i++){
+				image_array[j][i] = temparray[j][i];
+				if(image_array[j][i] == '\n' || image_array[j][i] == '\0'){
+					break;
+				}
+			}
+		}
+		return;
+		
+	}else{
+		for(int j = 0; j < *colm; j++){
+			for(int i = 0; i >= 0; i++){
+				image_array[j][i] = temparray[j][i];
+				if(image_array[j][i] == '\n' || image_array[j][i] == '\0'){
+					break;
+				}
+			}
+		}
+	}
+	
+return;
+}
+void brighten(char temparray[][X_SIZE], char chararray[][X_SIZE], char image_array[][X_SIZE], FILE* imagefileptr, int *colm, char *filename){
+	char choose;
+	//transer image_array to temparray
+	for(int j = 0; j < *colm; j++){
+		for(int i = 0; i >= 0; i++){
+			temparray[j][i] = image_array[j][i];
+			if(temparray[j][i] == '\n' || temparray[j][i] == '\0'){
+				break;
+			}
+		}
+	}
+	//brighten image_array	
+	for(int j = 0; j < *colm; j++){
+		for(int i = 0; i >= 0; i++){
+			if (temparray[j][i] == '0'){
+				image_array[j][i] = '1';
+			}else if (temparray[j][i] == '1'){
+				image_array[j][i] = '2';
+			}else if (temparray[j][i] == '2'){
+				image_array[j][i] = '3';
+			}else if (temparray[j][i] == '3'){
+				image_array[j][i] = '4';
+			}else if (temparray[j][i] == '4'){
+				image_array[j][i] = '4';
+			}
+			
+			if(temparray[j][i] == '\n' || temparray[j][i] == '\0'){
+				image_array[j][i] = temparray[j][i];
+				break;
+			}
+		}
+	}
+	
+	display(chararray, image_array, imagefileptr, colm);
+	printf("Would you like to save the file? (y/n) ");
+	scanf(" %c", &choose);
+	if(choose == 'y'){
+	
+		FILE* save;
+		
+		printf("What do you want to name the image file? (include the extension) ");
+		scanf("%s", filename);
+		save = fopen(filename, "w");
+		char newline = '\n';
+		for(int j = 0; j < *colm; j++){
+			for(int i = 0; i >= 0; i++){
+				fprintf(save, "%c", chararray[j][i]);
+				if(chararray[j][i] == '\n' || chararray[j][i] == '\0'){
+					break;
+				}
+			}
+		}
+		printf("\nImage successfully saved!\n\n");
+		fclose(save);
+		
+		for(int j = 0; j < *colm; j++){
+			for(int i = 0; i >= 0; i++){
+				image_array[j][i] = temparray[j][i];
+				if(image_array[j][i] == '\n' || image_array[j][i] == '\0'){
+					break;
+				}
+			}
+		}
+		return;
+		
+	}else{
+		for(int j = 0; j < *colm; j++){
+			for(int i = 0; i >= 0; i++){
+				image_array[j][i] = temparray[j][i];
+				if(image_array[j][i] == '\n' || image_array[j][i] == '\0'){
+					break;
+				}
+			}
+		}
+	}
+	
+return;
+}
+void crop(char chararray[][X_SIZE], char image_array[][X_SIZE], FILE* imagefileptr, int *colm, char *filename, int rowSize, int colSize){
+	
+	char choose;
 	int newleftcol, newrightcol, newtoprow, newbottomrow;
+	int index = 0;
+	int index2 = 0;
 	printf("The image you want to crop is %d x %d.\n", colSize, rowSize);
 	//printf("The row and column values start in the upper lefthand corner.\n");
 	printf("\nWhich column do you want to be the new left side? ");
@@ -134,8 +331,58 @@ void crop(int rowSize, int colSize, char image_array[][X_SIZE]){
 	printf("\nWhich row do you want to be the new bottom? ");
 	scanf("%d", &newbottomrow);
 	
+	int x = newrightcol - newleftcol + 1;
+	int y = newbottomrow - newtoprow + 1;
+	char newArray[x][y];
 	
 	
+	
+	
+	
+	display(chararray, newArray, imagefileptr, colm);
+	/*printf("Would you like to save the file? (y/n) ");
+	scanf(" %c", &choose);
+	if(choose == 'y'){
+	
+		FILE* save;
+		
+		printf("What do you want to name the image file? (include the extension) ");
+		scanf("%s", filename);
+		save = fopen(filename, "w");
+		char newline = '\n';
+		for(int j = 0; j < *colm; j++){
+			for(int i = 0; i >= 0; i++){
+				fprintf(save, "%c", chararray[j][i]);
+				if(chararray[j][i] == '\n' || chararray[j][i] == '\0'){
+					break;
+				}
+			}
+		}
+		printf("\nImage successfully saved!\n\n");
+		fclose(save);
+		
+		for(int j = 0; j < *colm; j++){
+			for(int i = 0; i >= 0; i++){
+				image_array[j][i] = temparray[j][i];
+				if(image_array[j][i] == '\n' || image_array[j][i] == '\0'){
+					break;
+				}
+			}
+		}
+		return;
+		
+	}else{
+		for(int j = 0; j < *colm; j++){
+			for(int i = 0; i >= 0; i++){
+				image_array[j][i] = temparray[j][i];
+				if(image_array[j][i] == '\n' || image_array[j][i] == '\0'){
+					break;
+				}
+			}
+		}
+	}
+	
+return;*/
 	
 }
 
